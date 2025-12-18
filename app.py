f4ebe5f54d0c0d3e613908940577146942cd8d1f
Salla Arun Reddy <218r1a66i3@gmail.com>
2025-12-18 22:58:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-
+import os
 
 app = Flask(__name__)
 
@@ -33,3 +33,4 @@ def contact():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)  
+
